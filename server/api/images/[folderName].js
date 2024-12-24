e3da4db02c6b756f9aa34c4056d1003073f35fa0
Papik <paja5913@gmail.com>
2024-12-24 13:50:22 +0100

    debug

--- server/api/images/[folderName].js
+++ server/api/images/[folderName].js
@@ -4,21 +4,24 @@ import path from 'path';
 export default defineEventHandler(async (event) => {
     try {
         const { folderName } = event.context.params;
+        console.log("Folder name:", folderName);
 
         if (!folderName) {
             throw createError({ statusCode: 400, message: "Folder name is required" });
         }
 
         const imageDirectory = path.resolve('./public/projects/images', folderName);
+        console.log("Resolved image directory:", imageDirectory);
 
-        // Safely read directory
         const files = await fs.promises.readdir(imageDirectory);
+        console.log("Files found:", files);
+
         const imageFiles = files.filter(file => /\.(jpg|jpeg|png|gif|svg)$/i.test(file));
+        console.log("Filtered image files:", imageFiles);
 
         return imageFiles.map(file => `/projects/images/${folderName}/${file}`);
     } catch (error) {
-        console.error('Error handling API request:', error);
+        console.error('Error handling API request:', error.message, error.stack);
         throw createError({ statusCode: 500, message: "Unable to process request" });
-    } //test
+    }
 });
-
