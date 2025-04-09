e926e255d0e2c51b26f264dc139f80a84a167841
Pavel-Kadanka <pavel.kadanka@outlook.cz>
2025-04-09 12:17:20 +0200

    added google analytics

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,7 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['vuetify-nuxt-module', '@nuxtjs/google-fonts'],
+  modules: ['vuetify-nuxt-module', '@nuxtjs/google-fonts', 'nuxt-gtag'],
   css: [
     '~/assets/css/styles.css'
   ],
@@ -12,6 +12,14 @@ export default defineNuxtConfig({
     },
     vuetifyOptions: {
       /* vuetify options */
+    }
+  },
+  gtag: {
+    enabled: process.env.NODE_ENV === 'production',
+    id: 'G-HBPLE7V1RS',
+    config: {
+      anonymize_ip: true,  // Protect user privacy
+      send_page_view: true // Auto-track route changes
     }
   },
   googleFonts: {
